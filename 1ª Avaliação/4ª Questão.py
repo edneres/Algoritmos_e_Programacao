6f6f8177f99f54e59f94fba59204f9ad5a7ecee7
Edivânia <edneres7@gmail.com>
2022-12-31 02:21:01 -0300

    Python

---
+++ 1ª Avaliação/4ª Questão.py
@@ -0,0 +1,15 @@
+n1 = int (input ('Digite um número inteiro: '))
+n2 = int (input ('Digite um número inteiro novamente: '))
+n3 = float (input ('Agora digite um número real: '))
+
+a = (2 * n1) * (n2/2)
+b = (3 * n1) + n3
+
+print('')
+print('a) O produto do dobro do primeiro com metade do segundo é {}'.format (a))
+print('b) A soma do triplo do primeiro com o terceiro é {}'.format (b))
+print('')
+
+#== Identificação ==#
+
+print("Aluna: Maria Edivânia\nCurso: Engenharia Elétrica, 2020.2")
