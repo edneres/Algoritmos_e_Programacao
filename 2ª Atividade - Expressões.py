d8f2feb4dfe8e0fc2b79e35b2f742bdd7d02a690
Edivânia <edneres7@gmail.com>
2022-12-31 02:23:15 -0300

    Python
    ▸ Escreva um programa que calcule a área de um retângulo. O usuário deve informar a altura e a largura;
    ▸ Escreva um programa que receba um valor em Celsius digitado pelo usuário, e converta em Fahrenheit;
    ▸ Escreva um programa que receba um valor em Fahrenheit digitado pelo usuário, e converta em Celsius.

---
+++ 2ª Atividade - Expressões.py
@@ -0,0 +1,30 @@
+#== 1º questão ==#
+
+x=input("Digite um valor para a base: ")
+b=float(x)
+y=input("Digite um valor para a altura: ")
+h=float(y)
+print("A área do retângulo é",b*h,"u.a.")
+
+
+#== 2ª questão ==#
+
+x=input("Digite uma temperatura em graus Celsius: ")
+c=float(x)
+fórmula=c*9/5+32
+f=fórmula
+print("O valor da temperatura em Fahrenheit é:",f,"°")
+
+
+#== 3ª questão ==#
+
+x=input("Digite uma temperatura em graus Fahrenheit: ")
+f=float(x)
+fórmula=(f-32)*5/9
+c=fórmula
+print("O valor da temperatura em Celsius é:",c,"°")
+
+
+#== Identificação ==#
+
+print("Aluna: Maria Edivânia\nCurso: Engenharia Elétrica, 2020.2")
