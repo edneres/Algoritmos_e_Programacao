015cee0ba53b18c476b3b99d3653b5c0c3705740
Edivânia <edneres7@gmail.com>
2022-12-31 02:25:40 -0300

    Python

---
+++ 1ª Avaliação/5ª Questão.py
@@ -0,0 +1,9 @@
+g = float (input ('Olá. Quanto você ganha por hora? '))
+h = float (input ('Certo... e quantas horas você trabalhou neste mês comercial de 30 dias? '))
+s = g * h
+print('Ah. Então, o seu salário será de R$ {:.2f}'.format (s))
+#sugestão: g = 5.29 e h = 208 dará aproximadamente o salário mínimo atual.
+
+
+#== Identificação ==#
+print("Aluna: Maria Edivânia\nCurso: Engenharia Elétrica, 2020.2")
